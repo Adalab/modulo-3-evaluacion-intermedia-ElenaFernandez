bce6d23dfaa304b7aaba15eae01db3dd520e78ed
ElenaFernandez <elena.fernandez92@gmail.com>
2022-01-18 09:54:18 +0100

    add function newContact

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,48 @@ import adalabersList from '../data/data.json';
 //import callToApi from '../services/api';
 
 function App() {
-  const [list, setList] = useState(adalabersList);
-  const tableHtml = list.results.map((adalaber, id) => (
+  const[list, setList] = useState(adalabersList.results);
+  const[name, setName]=useState('');
+  const[mentorin,setMentorin]=useState('');
+  const[speciality,setSpeciality]=useState('');
+
+  const tableHtml = list.map((adalaber, id) => (
     <tr key={id}>
       <td> {adalaber.name}</td>
       <td>{adalaber.counselor}</td>
       <td>{adalaber.speciality}</td>
     </tr>
   ));
+  
+  const handleSubmit=(event)=>{
+    event.preventDefault();
+  };
+  const handleNewName=(event)=>{
+    setName(event.target.value);
+  };
+
+  const handleNewMentorin=(event)=>{
+    setMentorin(event.target.value);
+  };
+  
+  const handleNewSpeciality=(event)=>{
+    setSpeciality(event.target.value);
+  };
+
+  const handleClick=(event)=>{
+    event.preventDefault();
+    const newAdalaber={
+      name:name,
+      counselor: mentorin,
+      speciality: speciality
+    };
+    setList([...list, newAdalaber]);
+    setName('');
+    setMentorin('');
+    setSpeciality('');
+    
+  };
+
   return (
     <div>
       <header>
@@ -28,6 +62,16 @@ function App() {
           </thead>
           <tbody>{tableHtml}</tbody>
         </table>
+        <form onSubmit={handleSubmit}>
+          <h2>Añadir una nueva Adalaber</h2>
+          <label htmlFor="name">Nombre:</label>
+          <input type="text" onChange={handleNewName} value={name} />
+          <label htmlFor="mentorin">Tutora</label>
+          <input type="text" onChange={handleNewMentorin} value={mentorin}/>
+          <label htmlFor="speciality">Especialidad</label>
+          <input type="text" onChange={handleNewSpeciality} value={speciality}/>
+         <input type="submit" onClick={handleClick} value= "Añadir una nueva Adalaber"/>  
+        </form>
       </main>
     </div>
   );
