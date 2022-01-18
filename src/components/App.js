7a67033aa14fe08175529d0fee004359c77b11fb
ElenaFernandez <elena.fernandez92@gmail.com>
2022-01-18 09:13:29 +0100

    add print list html

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,34 @@
-import { useState} from 'react';
+import { useState } from 'react';
 import '../styles/App.scss';
-import callToApi from '../services/api';
-
+import adalabersList from '../data/data.json';
+//import callToApi from '../services/api';
 
 function App() {
+  const [list, setList] = useState(adalabersList);
+  const tableHtml = list.results.map((adalaber, id) => (
+    <tr key={id}>
+      <td> {adalaber.name}</td>
+      <td>{adalaber.counselor}</td>
+      <td>{adalaber.speciality}</td>
+    </tr>
+  ));
   return (
     <div>
-      <h1>Hola mundo</h1>
+      <header>
+        <h1>Mis Adalabers</h1>
+      </header>
+      <main>
+        <table>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Tutora</th>
+              <th>Especialidad</th>
+            </tr>
+          </thead>
+          <tbody>{tableHtml}</tbody>
+        </table>
+      </main>
     </div>
   );
 }
